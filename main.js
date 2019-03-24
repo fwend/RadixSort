4e6442ff8ba221a19fbfdf4b548a4a4df3787d8f
Jos van Uden <josvanuden@gmail.com>
2019-03-24 10:39:45 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const arr = [170, 45, 75, 90, 802, 24, 2, 66, 999, 1000, 1001];
+
+const radixSort = a => {
+    const max = getMax(a);
+    const digits = Array(a.length).fill(0);
+
+    for (let exp = 1; exp <= max; exp *= 10) {
+        for (let j = 0; j < a.length; j++) {
+            digits[j] = Math.floor(a[j] / exp) % 10;
+        }
+        countingSort(digits, a);
+    }
+};
+
+const countingSort = (digits, a) => {
+    const copy = a.slice();
+    const freq = Array(10).fill(0);
+
+    for (let i = 0; i < digits.length; i++) {
+        const val = digits[i];
+        freq[val]++;
+    }
+
+    for (let i = 1; i < freq.length; i++) {
+        freq[i] += freq[i - 1];
+    }
+
+    for (let i = a.length - 1; i >= 0; i--) {
+        const val = digits[i];
+        const pos = freq[val] - 1;
+        a[pos] = copy[i];
+        freq[val]--;
+    }
+};
+
+const getMax = a => {
+    let max = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+};
+
+radixSort(arr);
+console.log(arr);
